fda07018056e18c177a095ad615aa0ca0fd3713a
C0derTang <tangchristopher111@gmail.com>
2024-05-31 16:14:14 -0700

    set up constants for kpop

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,17 +1,54 @@
-import ai from "../assets/ai.jpeg";
-import emilia from "../assets/emilia.jpeg";
-import hutao from "../assets/hutao.jpeg";
-import lena from "../assets/lena.jpeg";
-import mai from "../assets/mai.jpeg";
-import makima from "../assets/makima.jpeg";
-import marin from "../assets/marin.jpeg";
-import mikasa from "../assets/mikasa.jpeg";
-import mizuhara from "../assets/mizuhara.jpeg";
-import rem from "../assets/rem.jpeg";
-import ruka from "../assets/ruka.jpeg";
-import zerotwo from "../assets/zerotwo.jpeg";
-import speedwagon from "../assets/speedwagon.jpeg";
-import gojo from "../assets/gojo.jpeg";
+import ai from "../assets/waifus/ai.jpeg";
+import emilia from "../assets/waifus/emilia.jpeg";
+import hutao from "../assets/waifus/hutao.jpeg";
+import lena from "../assets/waifus/lena.jpeg";
+import mai from "../assets/waifus/mai.jpeg";
+import makima from "../assets/waifus/makima.jpeg";
+import marin from "../assets/waifus/marin.jpeg";
+import mikasa from "../assets/waifus/mikasa.jpeg";
+import mizuhara from "../assets/waifus/mizuhara.jpeg";
+import rem from "../assets/waifus/rem.jpeg";
+import ruka from "../assets/waifus/ruka.jpeg";
+import zerotwo from "../assets/waifus/zerotwo.jpeg";
+import speedwagon from "../assets/waifus/speedwagon.jpeg";
+import gojo from "../assets/waifus/gojo.jpeg";
+
+// i hate my life
+import aespa from "../assets/kpop/aespa.jpeg"
+import beomgyu from "../assets/kpop/beomgyu.jpeg"
+import bts from "../assets/kpop/bts.jpeg"
+import chaeyoung from "../assets/kpop/chaeyoung.jpeg"
+import dahyun from "../assets/kpop/dahyun.jpeg"
+import giselle from "../assets/kpop/giselle.jpeg"
+import hueningkai from "../assets/kpop/hueningkai.jpeg"
+import illit from "../assets/kpop/illit.jpeg"
+import iroha from "../assets/kpop/iroha.jpeg"
+import jeongyeon from "../assets/kpop/jeongyeon.jpeg"
+import jhope from "../assets/kpop/jhope.jpeg"
+import jihyo from "../assets/kpop/jihyo.jpeg"
+import jimin from "../assets/kpop/jimin.jpeg"
+import jin from "../assets/kpop/jin.jpeg"
+import jungkook from "../assets/kpop/jungkook.jpeg"
+import karina from "../assets/kpop/karina.jpeg"
+import mina from "../assets/kpop/mina.jpeg"
+import minju from "../assets/kpop/minju.jpeg"
+import moka from "../assets/kpop/moka.jpeg"
+import momo from "../assets/kpop/momo.jpeg"
+import nayeon from "../assets/kpop/nayeon.jpeg"
+import ningning from "../assets/kpop/ningning.jpeg"
+import rm from "../assets/kpop/rm.jpeg"
+import sana from "../assets/kpop/sana.jpeg"
+import soobin from "../assets/kpop/soobin.jpeg"
+import suga from "../assets/kpop/suga.jpeg"
+import taehyun from "../assets/kpop/taehyun.jpeg"
+import twice from "../assets/kpop/twice.jpeg"
+import txt from "../assets/kpop/txt.jpeg"
+import tzuyu from "../assets/kpop/tzuyu.jpeg"
+import v from "../assets/kpop/v.jpeg"
+import winter from "../assets/kpop/winter.jpeg"
+import wonhee from "../assets/kpop/wonhee.jpeg"
+import yeonjun from "../assets/kpop/yeonjun.jpeg"
+import yunah from "../assets/kpop/yunah.jpeg"
 
 const waifus = [
     {
@@ -72,4 +109,32 @@ const waifus = [
     }
 ]
 
-export {waifus};
+const idols = [
+    {
+        icon: bts,
+        id: 1,
+        members: [suga, rm, jungkook, jimin, jhope, jin, v]
+    },
+    {
+        icon: twice,
+        id: 2,
+        members: [momo, sana, nayeon, tzuyu, chaeyoung, jihyo, jeongyeon, mina, dahyun]
+    },
+    {
+        icon: aespa,
+        id: 3,
+        members: [giselle, karina, winter, ningning]
+    },
+    {
+        icon: illit,
+        id: 4,
+        members: [iroha, wonhee, minju, moka, yunah]
+    },
+    {
+        icon: txt,
+        id: 5,
+        members: [yeonjun, soobin, beomgyu, taehyun, hueningkai]
+    }
+]
+
+export {waifus, idols};
